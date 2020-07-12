ce2e61353f89f7d0c14a008bedf98415bb440b32
tianjian <836782146@qq.com>
2020-07-12 19:54:43 +0800

    测试浏览器title，测试通过

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+from django.conf.urls import url
+from lists import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home')
 ]
+
